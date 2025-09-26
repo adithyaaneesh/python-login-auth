36790477c7fbc8482c73c3d9aa862deca49a9f44
adithyaaneesh <adithyaaneesh29@gmail.com>
2025-09-26 19:04:54 +0530

    add user login and authentication

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -2,5 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.register),
+    path('', views.register,name='register'),
+    path('login/',views.user_login,name='login'),
+    path('logout/',views.user_logout,name='logout'),
+    path('dashboard/',views.dashboard,name='dashboard'),
 ]
--- auth_app/views.py
+++ auth_app/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        confirm_password = request.POST['confirmpassword']
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirmpassword')
 
         if password==confirm_password:
             if User.objects.filter(username=username).exists():
@@ -16,9 +18,30 @@ def register(request):
             user = User.objects.create_user(username=username, email=email,password=password)
             user.save()
             return render(request,'login.html')
-
         else:
             return render(request,'register.html',{'error':"Passsword are not same"})  
+
+    return render(request,'register.html')
+    
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('dashboard')
+        else:
+            return render(request,"login.html", {"error": "Invalid username or password"})
     else:
         return render(request,'login.html')
     
+def user_logout(request):
+    logout(request)
+    return redirect(request,'login')
+
+@login_required
+
+def dashboard(request):
+    return render(request,'dashboard.html')
