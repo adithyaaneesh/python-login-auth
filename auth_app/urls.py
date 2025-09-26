3d44d5ffc2254b1ec70fcad9caab4c3aeaf84d64
adithyaaneesh <adithyaaneesh29@gmail.com>
2025-09-26 18:13:49 +0530

    add user registration

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.sample),
+    path('', views.register),
 ]
--- auth_app/views.py
+++ auth_app/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
 
 # Create your views here.
 def register(request):
-    return
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirmpassword']
+
+        if password==confirm_password:
+            if User.objects.filter(username=username).exists():
+                return render(request,'register.html',{'error':"Username already exist"})
+            user = User.objects.create_user(username=username, email=email,password=password)
+            user.save()
+            return render(request,'login.html')
+
+        else:
+            return render(request,'register.html',{'error':"Passsword are not same"})  
+    else:
+        return render(request,'login.html')
+    
